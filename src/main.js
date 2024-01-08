4a09052ed3ec591d4db76f173af963ba38a40e3e
Vladislav Todorov <vladislav.todorov@abv.bg>
2024-01-08 21:07:14 +0200

    Limit hero in game-area
    // limit movemen otside of area

--- src/main.js
+++ src/main.js
@@ -8,15 +8,71 @@ const gameScore = document.querySelector('.game-score');
 gameStart.addEventListener('click', onGameStart);
 
 // Global key listeners
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+
+
+let keys = {};
+let player = {
+    x: 150,
+    y: 100,
+    width: 0,
+    height: 0
+};
+let game = {
+    speed: 2,
+    movingMultiplier: 4
+};
 
 // Game start function
 function onGameStart() {
     gameStart.classList.add('hide');
- 
+
     // Render Monika
     const monika = document.createElement('div');
     monika.classList.add('monika');
-    monika.style.top = '200px';
-    monika.style.left = '200px';
+    monika.style.top = player.y + 'px';
+    monika.style.left = player.x + 'px';
     gameArea.appendChild(monika);
+
+    player.width = monika.offsetWidth;
+    player.height = monika.offsetHeight;
+
+    // Game infinite loop
+    window.requestAnimationFrame(gameAction);
+}
+
+// Game loop function
+function gameAction() {
+    const monika = document.querySelector('.monika');
+
+    // Register user input
+    if (keys.ArrowUp && player.y > 0) {
+        player.y -= game.speed * game.movingMultiplier;
+    }
+    if (keys.ArrowDown && player.y + player.height < gameArea.offsetHeight) {
+        player.y += game.speed * game.movingMultiplier;
+    }
+    if (keys.ArrowLeft && player.x > 0) {
+        player.x -= game.speed * game.movingMultiplier;
+    }
+    if (keys.ArrowRight && player.x + player.width < gameArea.offsetWidth) {
+        player.x += game.speed * game.movingMultiplier;
+    }
+
+    // Apply movement
+    monika.style.top = player.y + 'px';
+    monika.style.left = player.x + 'px';
+
+    window.requestAnimationFrame(gameAction);
+}
+
+// Key handlers
+function onKeyDown(e) {
+    keys[e.code] = true;
+    // console.log(keys);
+}
+function onKeyUp(e) {
+    keys[e.code] = false;
+    // console.log(keys);
 }
